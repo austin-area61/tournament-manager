46f3090612762705fec2cee9b83a95aec917e273
austin-area61 <austinonyango.area61@gmail.com>
2024-09-18 07:43:24 +0300

    added a jsx structure for the form interface and container component from material UI.

--- tournament-manager-app/src/components/PlayerStatsForm.js
+++ tournament-manager-app/src/components/PlayerStatsForm.js
@@ -20,3 +20,96 @@ const PlayerStatsForm = () => {
     e.preventDefault();
     console.log(playerData);  // This is where we'll handle form submission later
   };
+  return (
+    <Container maxWidth="sm" style={{ marginTop: '20px' }}>
+      <Typography variant="h4" gutterBottom>
+        Add Player Stats
+      </Typography>
+      <form onSubmit={handleSubmit}>
+        <Grid container spacing={3}>
+          <Grid item xs={12}>
+            <TextField
+              fullWidth
+              label="Player Name"
+              name="name"
+              value={playerData.name}
+              onChange={handleChange}
+              variant="outlined"
+              required
+            />
+          </Grid>
+
+          <Grid item xs={12}>
+            <TextField
+              fullWidth
+              label="Win/Loss Ratio"
+              name="winLossRatio"
+              value={playerData.winLossRatio}
+              onChange={handleChange}
+              variant="outlined"
+              type="number"
+              required
+            />
+          </Grid>
+
+          <Grid item xs={12}>
+            <TextField
+              fullWidth
+              label="K/D Ratio"
+              name="kdRatio"
+              value={playerData.kdRatio}
+              onChange={handleChange}
+              variant="outlined"
+              type="number"
+              required
+            />
+          </Grid>
+
+          <Grid item xs={12}>
+            <TextField
+              fullWidth
+              label="Accuracy (%)"
+              name="accuracy"
+              value={playerData.accuracy}
+              onChange={handleChange}
+              variant="outlined"
+              type="number"
+              required
+            />
+          </Grid>
+
+          <Grid item xs={12}>
+            <TextField
+              fullWidth
+              label="Headshot Ratio (%)"
+              name="headshotRatio"
+              value={playerData.headshotRatio}
+              onChange={handleChange}
+              variant="outlined"
+              type="number"
+              required
+            />
+          </Grid>
+
+          <Grid item xs={12}>
+            <TextField
+              fullWidth
+              label="Preferred Weapon"
+              name="preferredWeapon"
+              value={playerData.preferredWeapon}
+              onChange={handleChange}
+              variant="outlined"
+              required
+            />
+          </Grid>
+
+          <Grid item xs={12}>
+            <Button variant="contained" color="primary" type="submit" fullWidth>
+              Add Player
+            </Button>
+          </Grid>
+        </Grid>
+      </form>
+    </Container>
+  );
+};
