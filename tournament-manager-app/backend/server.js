24e14e4d98d80f74652bed4a625af522c31a9474
austin-area61 <austinonyango.area61@gmail.com>
2024-09-18 18:28:59 +0300

    imported playerRoutes in the server file.

--- tournament-manager-app/backend/server.js
+++ tournament-manager-app/backend/server.js
@@ -4,10 +4,12 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const app = express();
 require('dotenv').config();
+const playerRoutes = require('./routes/playerRoutes');
 
 // Middleware
 app.use(express.json());
 app.use(cors());
+app.use('/api/players', playerRoutes);
 
 // MongoDB connection
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
