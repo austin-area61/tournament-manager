639d00870bb9aca256f38bf390af97da458d03ac
austin-area61 <austinonyango.area61@gmail.com>
2024-09-18 07:35:48 +0300

    main functional component for playerstatsform

--- tournament-manager-app/src/components/PlayerStatsForm.js
+++ tournament-manager-app/src/components/PlayerStatsForm.js
@@ -1,2 +1,12 @@
 import React, { useState } from 'react';
 import { TextField, Button, Grid, Typography, Container } from '@mui/material';
+
+const PlayerStatsForm = () => {
+  const [playerData, setPlayerData] = useState({
+    name: '',
+    winLossRatio: '',
+    kdRatio: '',
+    accuracy: '',
+    headshotRatio: '',
+    preferredWeapon: ''
+  });
